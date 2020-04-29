fa9a73499bb49233e26364dd6854d333283cb9b8
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-04-29 11:16:26 -0400

    Started js for entries and entry collection

---
+++ script.js
@@ -0,0 +1,14 @@
+// Creating an object for the journal entry
+const journalEntry = {
+    date: "test",
+    concepts: "test",
+    entry: "test",
+    mood: "test"
+}
+
+// Creating an array to store the journal entries.
+let entryCollection = []
+
+// Pushing a test object to the array
+entryCollection.push(journalEntry)
+// console.log(entryCollection);
