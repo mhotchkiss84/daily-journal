8d18c93e813723f81bd52faf7c0dfa7727d234c0
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-04-29 12:50:02 -0400

    Working on final functions for getting, storying, printing entries

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 // Creating an object for the journal entry
-const journalEntry = {
-  date: "test",
-  concepts: "test",
-  entry: "test",
-  mood: "test",
+let journalEntry = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: "",
 };
 
 // Creating an array to store the journal entries. I have some placeholder entries in there.
-let entryCollection = [
-  {
-    date: "test",
-    concepts: "test",
-    entry: "test",
-    mood: "test",
-  },
-  {
-    date: "testTwo",
-    concepts: "test",
-    entry: "test",
-    mood: "test",
-  },
-  {
-    date: "testTwo",
-    concepts: "test",
-    entry: "test",
-    mood: "test",
-  },
-];
+let entryCollection = [];
 
-// Pushing a test object to the array
-// entryCollection.push(journalEntry)
-
-// console.log(entryCollection);
+// Creating a function to grab all the users input and put them into an object
+function grabUserInput(){
+    journalEntry.date = document.querySelector(`#date`).value
+    journalEntry.concepts = document.querySelector(`#concepts-covered`).value
+    journalEntry.entry = document.querySelector(`#journal-entry`).value
+    journalEntry.mood = document.querySelector(`#mood`).value
+    entryCollection.push(journalEntry)
+    // console.log(entryCollection);
+}
 
 //Creating a function that takes an object and turns it into a journal entry
 // Might modigy layout of content in here later on to clean up/organize
-function buildJournalEntry(entry) {
-  document.querySelector(
-    `#entry-container`
-  ).innerHTML += `<section class="seperate-entries"><h3>Date: ${entry.date}</h3><p>Concepts covered: ${entry.concepts}</p><p>Entry: ${entry.entry}</p><p>Mood: ${entry.mood}</p>`;
+// Need to make seperate functions. Having the loop inside the build function does not work and makes duplicate posts
+function buildJournalEntry() {
+    for(let i = 0; i != entryCollection.length; i++){
+        document.querySelector(`#entry-container`).innerHTML += `<section class="single-entry">
+        <h3>Date: ${entryCollection[i].date}</h3>
+        <p>Concepts: ${entryCollection[i].concepts}</p>
+        </section>`
+    }
 }
-
-// In your Daily Journal form, add an event listener to the submit/ save button. The callback function for your event listener should do the following things:
-// Capture the values the user entered into your form fields.
-// Once you have the values of the text inputs in your JavaScript, build them into an object that looks like this
 
 // Creating an event listener for the submit entry button
 // Tried to do clg "test" instead of function after click and test would run before the button was pressed
-document
-  .querySelector(`#submit-entry-btn`)
-  .addEventListener("click", (eventAction) => {
-    console.log("test");
-  });
+document.querySelector(`#submit-entry-btn`).addEventListener("click", submitEvent => {
+    grabUserInput()
+    buildJournalEntry()
+});
